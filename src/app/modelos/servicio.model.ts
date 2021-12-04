50378f094cb65eb67935660029cb8abaefa5da09
RichiMarin <ricardmaherrera@gmail.com>
2021-12-04 16:52:46 -0500

    Commit CRUD todas las entidades

--- src/app/modelos/servicio.model.ts
+++ src/app/modelos/servicio.model.ts
@@ -1,11 +1,11 @@
 export class ServicioModelo{
     id?: String;
-    fecha?: Date;
+    fecha?: string;
     hora_inicio?: string;
     hora_fin?: string;
     placa_vehiculo?: string;
     nombre_conductor?: string;
-    dinero_recogido?: number;
+    dinero_recogido?: string;
     ruta?: string;
   }
   
